ca1f4f4d88f41b06e6d91aeb3a9aa71960e49b0b
Shreeya Jha <shreeyajha171@gmail.com>
2026-02-23 08:59:16 +0530

    Deleting OOPSBannerApp.class

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,13 +1,19 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println(String.join("", "  *****      *****      *****     ******"));
-        System.out.println(String.join("", " *     *    *     *    *     *    *     "));
-        System.out.println(String.join("", " *     *    *     *    *     *    *     "));
-        System.out.println(String.join("", " *     *    *     *    *****      ******"));
-        System.out.println(String.join("", " *     *    *     *    *               *"));
-        System.out.println(String.join("", " *     *    *     *    *               *"));
-        System.out.println(String.join("", "  *****      *****     *          ******"));
+        String[] bannerParts = new String[7];
 
+        bannerParts[0] = String.join("", "  *****      *****      *****     ******");
+        bannerParts[1] = String.join("", " *     *    *     *    *     *    *     ");
+        bannerParts[2] = String.join("", " *     *    *     *    *     *    *     ");
+        bannerParts[3] = String.join("", " *     *    *     *    *****      ******");
+        bannerParts[4] = String.join("", " *     *    *     *    *               *");
+        bannerParts[5] = String.join("", " *     *    *     *    *               *");
+        bannerParts[6] = String.join("", "  *****      *****     *          ******");
+
+        
+        for (String line : bannerParts) {
+            System.out.println(line);
+        }
     }
 }
