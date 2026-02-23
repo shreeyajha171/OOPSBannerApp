decc2fcbbee02416bcbb69d205e67124d4d510f8
Shreeya Jha <shreeyajha171@gmail.com>
2026-02-23 09:15:04 +0530

    Render OOPS as Banner using Inline Array Initialization

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,17 +1,17 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        String[] bannerParts = new String[7];
+        String[] bannerParts = {
+            String.join("", "  *****      *****      *****     ******"),
+            String.join("", " *     *    *     *    *     *    *     "),
+            String.join("", " *     *    *     *    *     *    *     "),
+            String.join("", " *     *    *     *    *****      ******"),
+            String.join("", " *     *    *     *    *               *"),
+            String.join("", " *     *    *     *    *               *"),
+            String.join("", "  *****      *****     *          ******")
+        };
 
-        bannerParts[0] = String.join("", "  *****      *****      *****     ******");
-        bannerParts[1] = String.join("", " *     *    *     *    *     *    *     ");
-        bannerParts[2] = String.join("", " *     *    *     *    *     *    *     ");
-        bannerParts[3] = String.join("", " *     *    *     *    *****      ******");
-        bannerParts[4] = String.join("", " *     *    *     *    *               *");
-        bannerParts[5] = String.join("", " *     *    *     *    *               *");
-        bannerParts[6] = String.join("", "  *****      *****     *          ******");
-
-        
+     
         for (String line : bannerParts) {
             System.out.println(line);
         }
